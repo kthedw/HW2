8c953c7417e4c551b5999a2c13d84a5d76221844
kit1987 <68110933+kit1987@users.noreply.github.com>
2020-10-29 00:51:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class Employee
+{
+    public static void Main()
+    {
+        string path = @"c:\_kthedw\VS_projects\HW2\Employee.csv";
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
